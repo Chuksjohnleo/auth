8f2635278cd1fde8a5a45a5757e6b74e7682d4bf
Chuksjohnleo <nwanonenyichukwuka@gmail.com>
2024-06-04 20:39:58 +0100

    Favicon added

--- src/header.php
+++ src/header.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
 <link href="public/css/tailwind.css" rel="stylesheet">
 <script src="public/js/index.js"></script>
